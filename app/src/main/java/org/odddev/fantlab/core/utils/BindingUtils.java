6b1f875683154302de82e6208f94598864122bde
kenrube <kenrube@gmail.com>
2016-11-28 00:45:14 +0700

    #fix NavDrawer header's ConstraintLayout

--- app/src/main/java/org/odddev/fantlab/core/utils/BindingUtils.java
+++ app/src/main/java/org/odddev/fantlab/core/utils/BindingUtils.java
@@ -2,13 +2,18 @@ package org.odddev.fantlab.core.utils;
 
 import android.databinding.BindingAdapter;
 import android.databinding.BindingConversion;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.design.widget.TextInputLayout;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.DrawableRequestBuilder;
 import com.bumptech.glide.Glide;
+
+import org.odddev.fantlab.R;
 
 /**
  * @author kenrube
@@ -40,6 +45,17 @@ public class BindingUtils {
             builder = builder.transform(new CircleTransform(imageView.getContext()));
         }
         builder.into(imageView);
+    }
+
+    @BindingAdapter("font")
+    public static void bindFont(TextView textView, final String fontName) {
+        String robotoMedium = textView.getContext().getString(R.string.roboto_medium);
+        if (fontName.equals(robotoMedium) && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            textView.setTypeface(Typeface.createFromAsset(
+                    textView.getContext().getAssets(), "fonts/" + robotoMedium + ".ttf"));
+        } else {
+            textView.setTypeface(Typeface.create(fontName, Typeface.NORMAL));
+        }
     }
 
     @BindingConversion
