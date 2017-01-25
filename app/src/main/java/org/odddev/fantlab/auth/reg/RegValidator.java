4e40f4c7d48f3e1f7236fc36057ca2f302db3fff
kenrube <kenrube@gmail.com>
2017-01-25 07:30:40 +0700

    #2.1 add location mode switcher, skype, icq fields

--- app/src/main/java/org/odddev/fantlab/auth/reg/RegValidator.java
+++ app/src/main/java/org/odddev/fantlab/auth/reg/RegValidator.java
@@ -19,7 +19,11 @@ public class RegValidator extends Validator {
             FIELD.USERNAME,
             FIELD.PASSWORD,
             FIELD.EMAIL,
-            FIELD.BIRTH_DATE})
+            FIELD.BIRTH_DATE,
+            FIELD.WEB_PAGE,
+            FIELD.SKYPE,
+            FIELD.ICQ
+    })
     public @interface FIELD {
 
         int USERNAME = 0;
@@ -27,6 +31,8 @@ public class RegValidator extends Validator {
         int EMAIL = 2;
         int BIRTH_DATE = 3;
         int WEB_PAGE = 4;
+        int SKYPE = 5;
+        int ICQ = 6;
     }
 
     private Resources resources;
@@ -43,65 +49,47 @@ public class RegValidator extends Validator {
     @Override
     protected void validate(Integer field) {
         String value = fields.get(field);
-        switch (field) {
-            case FIELD.USERNAME: {
-                fieldErrors.put(FIELD.USERNAME, getUsernameError(value));
-                break;
-            }
-            case FIELD.PASSWORD: {
-                fieldErrors.put(FIELD.PASSWORD, getPasswordError(value));
-                break;
-            }
-            case FIELD.EMAIL: {
-                fieldErrors.put(FIELD.EMAIL, getEmailError(value));
-                break;
-            }
-            case FIELD.BIRTH_DATE: {
-                fieldErrors.put(FIELD.BIRTH_DATE, getBirthDateError(value));
-                break;
-            }
-            case FIELD.WEB_PAGE: {
-                fieldErrors.put(FIELD.WEB_PAGE, getUrlError(value));
-                break;
-            }
-        }
+        fieldErrors.put(field, getFieldError(field, value));
     }
 
     @Override
     protected boolean areFieldsValid() {
         boolean result = true;
-        for (@FIELD int i = 0; i < 4; i++) {
+        for (@FIELD int i = 0; i < 7; i++) {
             validate(i);
             if (fieldErrors.get(i) != null) result = false;
         }
         return result;
     }
 
-    private String getUsernameError(String username) {
-        return username == null || username.trim().isEmpty()
-                ? resources.getString(R.string.auth_username_empty)
-                : null;
-    }
-
-    private String getPasswordError(String password) {
-        return password == null || password.trim().isEmpty()
-                ? resources.getString(R.string.auth_password_empty)
-                : null;
-    }
-
-    private String getEmailError(String email) {
-        return email == null || !Patterns.EMAIL_ADDRESS.matcher(email).matches()
-                ? resources.getString(R.string.register_email_incorrect)
-                : null;
-    }
-
-    private String getBirthDateError(String birthDate) {
-        return null;
-    }
-
-    private String getUrlError(String url) {
-        return url == null || !Patterns.WEB_URL.matcher(url).matches()
-                ? resources.getString(R.string.register_url_incorrect)
-                : null;
+    private String getFieldError(@FIELD int field, String value) {
+        switch (field) {
+            case FIELD.USERNAME:
+                return value == null || value.trim().isEmpty()
+                        ? resources.getString(R.string.auth_username_empty)
+                        : null;
+            case FIELD.PASSWORD:
+                return value == null || value.trim().isEmpty()
+                        ? resources.getString(R.string.auth_password_empty)
+                        : null;
+            case FIELD.EMAIL:
+                return value == null || !Patterns.EMAIL_ADDRESS.matcher(value).matches()
+                        ? resources.getString(R.string.register_email_incorrect)
+                        : null;
+            case FIELD.BIRTH_DATE:
+                return null;
+            case FIELD.WEB_PAGE:
+                return value == null || !Patterns.WEB_URL.matcher(value).matches()
+                        ? resources.getString(R.string.register_url_incorrect)
+                        : null;
+            case FIELD.SKYPE:
+                return null;
+            case FIELD.ICQ:
+                return value == null || value.length() < 5 || value.length() > 9
+                        ? resources.getString(R.string.register_icq_incorrect)
+                        : null;
+            default:
+                return null;
+        }
     }
 }
