55d28380ba9c3bd2a45788481306b0070a055005
yourMichael <mikeslifix@gmail.com>
2017-01-31 05:49:06 +0500

    #full kotlin

--- app/src/main/java/org/odddev/fantlab/home/HomeProvider.kt
+++ app/src/main/java/org/odddev/fantlab/home/HomeProvider.kt
@@ -1,7 +1,7 @@
 package org.odddev.fantlab.home
 
 import org.odddev.fantlab.core.di.Injector
-import org.odddev.fantlab.core.rx.ISchedulersResolver
+import org.odddev.fantlab.core.rx.applyDefaultSchedulers
 import org.odddev.fantlab.core.storage.StorageManager
 
 import javax.inject.Inject
@@ -19,16 +19,13 @@ class HomeProvider : IHomeProvider {
 	@Inject
 	lateinit var storageManager: StorageManager
 
-	@Inject
-	lateinit var schedulersResolver: ISchedulersResolver
-
 	init {
 		Injector.getAppComponent().inject(this)
 	}
 
 	override fun getUserName(): Single<String> {
 		return Single.just<String>(storageManager.loadUsername())
-				.compose(schedulersResolver.applyDefaultSchedulers<String>())
+				.applyDefaultSchedulers()
 	}
 
 	override fun clearCookie() {
