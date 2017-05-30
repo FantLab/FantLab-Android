ec9aec1b8939f76e4c92770d12eb59b0483d41ea
kenrube <kenrube@gmail.com>
2017-05-30 11:46:30 +0700

    format text [still incomplete]

--- app/src/main/java/org/odddev/fantlab/core/utils/FormatUtils.kt
+++ app/src/main/java/org/odddev/fantlab/core/utils/FormatUtils.kt
@@ -11,15 +11,22 @@ import android.text.Spanned
 @Suppress("DEPRECATION")
 fun CharSequence.formatText(): Spanned {
     val baseUrl = "https://fantlab.ru"
-    val simpleTag = "b|i|u|s|p"
+    //val simpleTag = "b|i|u|s|p"
     val internalTag = "autor|award|work|edition|person|user|art|dictor|series|film|translator"
     val urlTag = "url|URL"
     val html = this
-            .replace("\\[($simpleTag)](.*)\\[/\\1]".toRegex(), "<$1>$2</$1>")
-            .replace("\\[($internalTag)=(\\d+)](.*)\\[/\\1]".toRegex(), "<a href=\"$baseUrl/$1$2\">$3</a>")
-            .replace("\\[pub=(\\d+)](.*)\\[/pub]".toRegex(), "<a href=\"$baseUrl/publisher$1\">$2</a>")
-            .replace("\\[link=(.*)](.*)\\[/link]".toRegex(), "<a href=\"$baseUrl/$1\">$2</a>")
-            .replace("\\[($urlTag)=(.*)](.*)\\[/\\1]".toRegex(), "<a href=\"$2\">$3</a>")
+            // простые теги могут быть вложенными, так что обращение \\1 не сработает для вложенных тегов
+            // как обойти, пока не придумал
+            //.replace("\\[($simpleTag)](.*?)\\[/\\1]".toRegex(), "<$1>$2</$1>")
+            .replace("\\[b](.*?)\\[/b]".toRegex(), "<b>$1</b>")
+            .replace("\\[i](.*?)\\[/i]".toRegex(), "<i>$1</i>")
+            .replace("\\[u](.*?)\\[/u]".toRegex(), "<u>$1</u>")
+            .replace("\\[s](.*?)\\[/s]".toRegex(), "<s>$1</s>")
+            .replace("\\[p](.*?)\\[/p]".toRegex(), "<p>$1</p>")
+            .replace("\\[($internalTag)=(\\d+)](.*?)\\[/\\1]".toRegex(), "<a href=\"$baseUrl/$1$2\">$3</a>")
+            .replace("\\[pub=(\\d+)](.*?)\\[/pub]".toRegex(), "<a href=\"$baseUrl/publisher$1\">$2</a>")
+            .replace("\\[link=(.*)](.*?)\\[/link]".toRegex(), "<a href=\"$baseUrl/$1\">$2</a>")
+            .replace("\\[($urlTag)=(.*)](.*?)\\[/\\1]".toRegex(), "<a href=\"$2\">$3</a>")
             .replace("[PHOTO]", "")
             .replace("\r?\n".toRegex(), "<br/>")
 
