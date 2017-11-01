11b5c708931b6f65db1972efa823a586805a532a
kenrube <kenrube@gmail.com>
2017-11-01 07:48:41 +0700

    remove requery

--- app/src/main/kotlin/org/odddev/fantlab/autors/autor/IAuthorProvider.kt
+++ app/src/main/kotlin/org/odddev/fantlab/autors/autor/IAuthorProvider.kt
@@ -1,9 +1,8 @@
 package org.odddev.fantlab.autors.autor
 
 import io.reactivex.Observable
-import org.odddev.fantlab.core.db.AuthorRecord
 
 interface IAuthorProvider {
 
-	fun getAuthor(id: Int): Observable<Iterable<AuthorRecord>>
+	fun getAuthor(id: Int): Observable<Void>
 }
--- app/src/main/kotlin/org/odddev/fantlab/core/di/AppModule.kt
+++ app/src/main/kotlin/org/odddev/fantlab/core/di/AppModule.kt
@@ -3,25 +3,12 @@ package org.odddev.fantlab.core.di
 import android.content.Context
 import dagger.Module
 import dagger.Provides
-import io.requery.Persistable
-import io.requery.android.sqlite.DatabaseSource
-import io.requery.reactivex.KotlinReactiveEntityStore
-import io.requery.sql.KotlinEntityDataStore
-import io.requery.sql.TableCreationMode
-import org.odddev.fantlab.Models
 import org.odddev.fantlab.core.network.INetworkChecker
 import org.odddev.fantlab.core.network.NetworkChecker
 import javax.inject.Singleton
 
 @Module
 class AppModule(internal val context: Context) {
-
-	private val dataStore: KotlinReactiveEntityStore<Persistable> by lazy {
-		val source = DatabaseSource(context, Models.DEFAULT, 1)
-		source.setTableCreationMode(TableCreationMode.DROP_CREATE)
-		source.setLoggingEnabled(true)
-		KotlinReactiveEntityStore<Persistable>(KotlinEntityDataStore(source.configuration))
-	}
 
 	@Singleton
 	@Provides
@@ -30,8 +17,4 @@ class AppModule(internal val context: Context) {
 	@Singleton
 	@Provides
 	fun provideNetworkChecker(): INetworkChecker = NetworkChecker()
-
-	@Singleton
-	@Provides
-	fun provideRequery(): KotlinReactiveEntityStore<Persistable> = dataStore
 }
