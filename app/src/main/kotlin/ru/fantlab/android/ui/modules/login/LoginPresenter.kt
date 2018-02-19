eaffe6d4c7fbe81e51625473474d5aeae72deb68
kenrube <kenrube@gmail.com>
2018-02-19 16:18:34 +0700

    #27 обработка неудачной авторизации

--- app/src/main/kotlin/ru/fantlab/android/ui/modules/login/LoginPresenter.kt
+++ app/src/main/kotlin/ru/fantlab/android/ui/modules/login/LoginPresenter.kt
@@ -27,6 +27,11 @@ class LoginPresenter : BasePresenter<LoginMvp.View>(), LoginMvp.Presenter {
 		if (!usernameIsEmpty && !passwordIsEmpty) {
 			try {
 				makeRestCall(LoginProvider.getLoginRestService().login(username, password), Consumer {
+					val location = it.headers().values("location")
+					if (location.isNotEmpty() && location[0] == "/loginincorrect") {
+						showSignInFailed()
+						return@Consumer
+					}
 					onTokenResponse(username, it)
 				})
 			} catch (e: Exception) {
@@ -40,6 +45,11 @@ class LoginPresenter : BasePresenter<LoginMvp.View>(), LoginMvp.Presenter {
 			if (!InputHelper.isEmpty(it) && it.startsWith("fl_s")) {
 				PrefGetter.setToken(it.substring(0, it.indexOf(";")))
 				makeRestCall(RestProvider.getUserService().getUserId(username), Consumer {
+					if (it.userId == 0) {
+						// по идее, здесь мы оказаться не можем ни в коем случае
+						showSignInFailed()
+						return@Consumer
+					}
 					makeRestCall(RestProvider.getUserService().getLoggedUser(it.userId), Consumer {
 						onUserResponse(it)
 					})
@@ -59,7 +69,11 @@ class LoginPresenter : BasePresenter<LoginMvp.View>(), LoginMvp.Presenter {
 			)
 			return
 		} else {
-			sendToView { it.showMessage(R.string.error, R.string.failed_login) }
+			showSignInFailed()
 		}
+	}
+
+	private fun showSignInFailed() {
+		sendToView { it.showMessage(R.string.error, R.string.failed_login) }
 	}
 }
