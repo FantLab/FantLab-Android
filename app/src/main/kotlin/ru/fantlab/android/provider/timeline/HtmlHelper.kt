3b565b9a67f0f2671b5c678ce03597a6ccfa1c35
Hombre <Ilyakokhan@gmail.com>
2018-09-07 18:34:42 +0300

    #93 Делать обрезку описания ворка

--- app/src/main/kotlin/ru/fantlab/android/provider/timeline/HtmlHelper.kt
+++ app/src/main/kotlin/ru/fantlab/android/provider/timeline/HtmlHelper.kt
@@ -18,7 +18,7 @@ object HtmlHelper {
 
 	fun htmlIntoTextView(textView: TextView, html: String, width: Int) {
 		registerClickEvent(textView)
-		textView.text = initHtml(textView, width).fromHtml(html)
+		textView.text = initHtml(textView, width).fromHtml(format(html).toString())
 	}
 
 	private fun registerClickEvent(textView: TextView) {
@@ -77,6 +77,48 @@ object HtmlHelper {
 			Color.parseColor("#22252A")
 		} else {
 			Color.parseColor("#EEEEEE")
+		}
+	}
+
+	private const val BREAK = "<br>"
+	private const val PARAGRAPH_START = "<p>"
+	private const val PARAGRAPH_END = "</p>"
+
+	fun format(html: String?): CharSequence {
+		if (html == null || html.isEmpty()) return ""
+		val formatted = StringBuilder(html)
+		if (replace(formatted, PARAGRAPH_START, BREAK)) replace(formatted, PARAGRAPH_END, BREAK)
+		trim(formatted)
+		return formatted
+	}
+
+	private fun replace(input: StringBuilder, from: String, to: String): Boolean {
+		var start = input.indexOf(from)
+		if (start == -1) return false
+		val fromLength = from.length
+		val toLength = to.length
+		while (start != -1) {
+			input.replace(start, start + fromLength, to)
+			start = input.indexOf(from, start + toLength)
+		}
+		return true
+	}
+
+	private fun trim(input: StringBuilder) {
+		var length = input.length
+		val breakLength = BREAK.length
+		while (length > 0) {
+			if (input.indexOf(BREAK) == 0)
+				input.delete(0, breakLength)
+			else if (length >= breakLength && input.lastIndexOf(BREAK) == length - breakLength)
+				input.delete(length - breakLength, length)
+			else if (Character.isWhitespace(input[0]))
+				input.deleteCharAt(0)
+			else if (Character.isWhitespace(input[length - 1]))
+				input.deleteCharAt(length - 1)
+			else
+				break
+			length = input.length
 		}
 	}
 }
