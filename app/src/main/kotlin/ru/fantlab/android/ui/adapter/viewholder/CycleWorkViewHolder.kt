597cb82901a196b369a0264bf315dc8fdd03105a
Hombre <Ilyakokhan@gmail.com>
2018-09-14 20:22:22 +0300

    фикс ошибочных элементов в списке библиографии

--- app/src/main/kotlin/ru/fantlab/android/ui/adapter/viewholder/CycleWorkViewHolder.kt
+++ app/src/main/kotlin/ru/fantlab/android/ui/adapter/viewholder/CycleWorkViewHolder.kt
@@ -2,8 +2,7 @@ package ru.fantlab.android.ui.adapter.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import butterknife.BindView
-import butterknife.ButterKnife
+import android.widget.TextView
 import ru.fantlab.android.R
 import ru.fantlab.android.data.dao.model.CycleWork
 import ru.fantlab.android.helper.InputHelper
@@ -14,20 +13,11 @@ import ru.fantlab.android.ui.widgets.treeview.TreeViewBinder
 import java.text.NumberFormat
 
 class CycleWorkViewHolder : TreeViewBinder<CycleWorkViewHolder.ViewHolder>() {
-
-	@BindView(R.id.authors) lateinit var authors: FontTextView
-	@BindView(R.id.title) lateinit var title: FontTextView
-	@BindView(R.id.description) lateinit var description: FontTextView
-	@BindView(R.id.year) lateinit var year: FontTextView
-	@BindView(R.id.rating) lateinit var rating: FontTextView
-	@BindView(R.id.responses) lateinit var responses: FontTextView
-	@BindView(R.id.mark) lateinit var mark: FontTextView
 	private val numberFormat = NumberFormat.getNumberInstance()
 
 	override val layoutId = R.layout.author_cycle_work_row_item
 
 	override fun provideViewHolder(itemView: View): ViewHolder {
-		ButterKnife.bind(this, itemView)
 		return ViewHolder(itemView)
 	}
 
@@ -35,14 +25,15 @@ class CycleWorkViewHolder : TreeViewBinder<CycleWorkViewHolder.ViewHolder>() {
 			holder: RecyclerView.ViewHolder, position: Int, node: TreeNode<*>, onTreeNodeListener: TreeViewAdapter.OnTreeNodeListener?
 	) {
 		val work = (node.content as CycleWork).work
+		(holder as CycleWorkViewHolder.ViewHolder)
 			if (work.authors.size > 1) {
-				authors.text = work.authors.joinToString { it.name }
-				authors.visibility = View.VISIBLE
+				holder.authors.text = work.authors.joinToString { it.name }
+				holder.authors.visibility = View.VISIBLE
 			} else {
-				authors.visibility = View.GONE
+				holder.authors.visibility = View.GONE
 			}
 
-			title.text = if (work.name.isNotEmpty()) {
+		holder.title.text = if (work.name.isNotEmpty()) {
 				if (work.nameOrig.isNotEmpty()) {
 					String.format("%s / %s", work.name, work.nameOrig)
 				} else {
@@ -53,33 +44,41 @@ class CycleWorkViewHolder : TreeViewBinder<CycleWorkViewHolder.ViewHolder>() {
 			}
 
 			if (!InputHelper.isEmpty(work.description)) {
-				description.text = work.description?.let { work.description.replace("\\[(.*?)]".toRegex(), "") }
-			} else description.visibility = View.GONE
+				holder.description.text = work.description?.let { work.description.replace("\\[(.*?)]".toRegex(), "") }
+			} else holder.description.visibility = View.GONE
 
 			if (!InputHelper.isEmpty(work.year)) {
-				year.text = work.year.toString()
-			} else year.visibility = View.GONE
+				holder.year.text = work.year.toString()
+			} else holder.year.visibility = View.GONE
 
 			if (work.responses != null && work.responses != "0") {
-				responses.text = work.responses
-				responses.visibility = View.VISIBLE
-			} else responses.visibility = View.GONE
+				holder.responses.text = work.responses
+				holder.responses.visibility = View.VISIBLE
+			} else holder.responses.visibility = View.GONE
 
 			if (work.votersCount != 0 && work.rating != null) {
 
-				rating.text = String.format("%s / %s",
+				holder.rating.text = String.format("%s / %s",
 						numberFormat.format(work.rating),
 						numberFormat.format(work.votersCount))
-				rating.visibility = View.VISIBLE
+				holder.rating.visibility = View.VISIBLE
 			} else {
-				rating.visibility = View.GONE
+				holder.rating.visibility = View.GONE
 			}
 		val myMark = (node.content as CycleWork).mark
 		if (myMark != null || myMark == 0) {
-			mark.text = myMark.toString()
-			mark.visibility = View.VISIBLE
-		} else mark.visibility = View.GONE
+			holder.mark.text = myMark.toString()
+			holder.mark.visibility = View.VISIBLE
+		} else holder.mark.visibility = View.GONE
 	}
 
-	inner class ViewHolder(rootView: View) : TreeViewBinder.ViewHolder(rootView)
+	inner class ViewHolder(rootView: View) : TreeViewBinder.ViewHolder(rootView){
+		var title: TextView = rootView.findViewById<View>(R.id.title) as FontTextView
+		var authors: TextView = rootView.findViewById<View>(R.id.authors) as FontTextView
+		var description: TextView = rootView.findViewById<View>(R.id.description) as FontTextView
+		var year: TextView = rootView.findViewById<View>(R.id.year) as FontTextView
+		var rating: TextView = rootView.findViewById<View>(R.id.rating) as FontTextView
+		var responses: TextView = rootView.findViewById<View>(R.id.responses) as FontTextView
+		var mark: TextView = rootView.findViewById<View>(R.id.mark) as FontTextView
+	}
 }
