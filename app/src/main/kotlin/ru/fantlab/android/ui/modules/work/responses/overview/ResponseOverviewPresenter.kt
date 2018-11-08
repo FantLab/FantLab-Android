7f5b0230cfaee215e5cbf7105be8fa29cf055932
Hombre <ilyakokhan@gmail.com>
2018-11-08 10:02:19 +0300

    #122 Проверки граничных условий (просмотр отзыва)

--- app/src/main/kotlin/ru/fantlab/android/ui/modules/work/responses/overview/ResponseOverviewPresenter.kt
+++ app/src/main/kotlin/ru/fantlab/android/ui/modules/work/responses/overview/ResponseOverviewPresenter.kt
@@ -1,8 +1,10 @@
 package ru.fantlab.android.ui.modules.work.responses.overview
 
 import io.reactivex.functions.Consumer
+import ru.fantlab.android.R
 import ru.fantlab.android.data.dao.model.Response
 import ru.fantlab.android.data.dao.response.VoteResponse
+import ru.fantlab.android.helper.PrefGetter
 import ru.fantlab.android.provider.rest.DataManager
 import ru.fantlab.android.ui.base.mvp.presenter.BasePresenter
 
@@ -21,18 +23,27 @@ class ResponseOverviewPresenter : BasePresenter<ResponseOverviewMvp.View>(),
 	override fun getResponses(): Response = response!!
 
 	fun onSendVote(item: Response, voteType: String){
-		makeRestCall(DataManager.sendResponseVote(item.id, voteType)
-				.map { it.get() }
-				.toObservable(),
-				Consumer { response ->
-					val result = VoteResponse.Parser().parse(response)
-					if (result != null) {
-						sendToView { view ->
-							view.onSetVote(result.votesCount)
-						}
-					} else {
-						sendToView { it.showErrorMessage(response) }
-					}
-				})
+		makeRestCall(
+				DataManager.getUser(PrefGetter.getLoggedUser()?.id!!)
+						.map { it.get() }
+						.toObservable(),
+				Consumer { it ->
+					if (it.user.level >= 200) {
+						makeRestCall(DataManager.sendResponseVote(item.id, voteType)
+								.map { it.get() }
+								.toObservable(),
+								Consumer { response ->
+									val result = VoteResponse.Parser().parse(response)
+									if (result != null) {
+										sendToView { view ->
+											view.onSetVote(result.votesCount)
+										}
+									} else {
+										sendToView { it.showErrorMessage(response) }
+									}
+								})
+					} else view?.showMessage(R.string.error, R.string.cannotvote_novice)
+				}
+		)
 	}
 }
