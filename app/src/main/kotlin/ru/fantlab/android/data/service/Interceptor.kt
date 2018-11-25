73ec66322df99244f115e0dddc29a15806096734
kenrube <kenrube@gmail.com>
2018-11-25 15:20:53 +0300

    #85 не сохраняем пагинационные запросы дальше 1-й страницы

--- app/src/main/kotlin/ru/fantlab/android/data/service/Interceptor.kt
+++ app/src/main/kotlin/ru/fantlab/android/data/service/Interceptor.kt
@@ -1,5 +1,6 @@
 package ru.fantlab.android.data.service
 
+import android.net.Uri
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.core.isSuccessful
@@ -11,14 +12,18 @@ fun dbResponseInterceptor() =
 		{ next: (Request, Response) -> Response ->
 			{ req: Request, res: Response ->
 				if (res.isSuccessful) {
-					val response = ru.fantlab.android.data.db.response.Response(
-							req.path,
-							PrefGetter.getSessionUserId(),
-							String(res.data),
-							BuildConfig.API_VERSION,
-							System.currentTimeMillis()
-					)
-					DbProvider.mainDatabase.responseDao().save(response)
+					val pageValue = Uri.parse(req.path).getQueryParameter("page")
+					// пагинационные запросы для страниц дальше 1-й не сохраняем
+					if (pageValue == null || pageValue == "1") {
+						val response = ru.fantlab.android.data.db.response.Response(
+								req.path,
+								PrefGetter.getSessionUserId(),
+								String(res.data),
+								BuildConfig.API_VERSION,
+								System.currentTimeMillis()
+						)
+						DbProvider.mainDatabase.responseDao().save(response)
+					}
 				}
 				next(req, res)
 			}
